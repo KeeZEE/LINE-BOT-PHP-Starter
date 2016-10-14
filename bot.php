8ee600bea65eb9d4e5db8d0e5328b67d9f43a92a
KiiE <kornkrit@odeosolution.com>
2016-10-14 13:14:04 +0700

    Updated line bot

--- bot.php
+++ bot.php
@@ -1,2 +1,51 @@
 <?php
-echo "I am a bot";
+$access_token = 'pHjOVvc0zTnnyLhYtOE5g1GfgXvx6AE3jLmf7AnK/lYogZDykPRQxp+1CjcfGJXAT9I7LZQIRPpJ/B8cejE4xHNPUkk47qQodwxRKmDEp4D1nJhcOW3CQ0VkfSZ3sIhU67hs0Im6LD/HwlKds9KMXQdB04t89/1O/w1cDnyilFU=';
+$proxy = 'http://fixie:7RQBS3KxVGbKHKs@velodrome.usefixie.com:80';
+$proxyauth = 'kornkrit.leel@gmail.com:Kornkrit1234';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get text sent
+            $text = $event['message']['text'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $text
+            ];
+
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result . "\r\n";
+        }
+    }
+}
+echo "OK";
